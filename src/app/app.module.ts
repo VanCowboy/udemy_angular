9816e917aa16dd60fff2473f8a679e5861cc63cc
jmorgan <jmorgan>
2018-08-08 13:10:02 -0700

    Validation Methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -11,20 +11,25 @@ import { AuthorComponent } from './author/author.component';
 import { AuthorsService } from './authors.service';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { InputFormatDirective } from './input-format.directive';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupFormComponent,
     CoursesComponent,
     CourseComponent,
     AuthorsComponent,
     AuthorComponent,
     FavoriteComponent,
-    TitleCasePipe
+    TitleCasePipe,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService,
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
+export class FavoriteComponent {
+  isSelected: boolean;
   title: string;
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onClick() {
-    this.isFavorite = !this.isFavorite;
+    this.isSelected = !this.isSelected;
+    //this.onClick.emit({newValue: this.isSelected});
   }
+}
 
+export interface FavoriteChangeEventArgs{
+  newValue: boolean;
 }
